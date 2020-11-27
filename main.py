dbfda1cc0f245aef355e0acaf6c613618e54856c
isus-ipanienko <57775222+isus-ipanienko@users.noreply.github.com>
2020-11-27 23:02:46 +0100

    Add files via upload
    rework okienek

---
+++ main.py
@@ -0,0 +1,31 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+import auto_window
+import manual_window
+
+
+class Controller:
+
+    def __init__(self):
+        pass
+
+    def show_manual(self):
+        self.manual = manual_window.ManualWindow()
+        self.manual.switch_window.connect(self.show_auto)
+        self.manual.show()
+
+    def show_auto(self):
+        self.auto = auto_window.AutoWindow()
+        self.auto.switch_window.connect(self.show_manual)
+        self.auto.show()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    controller = Controller()
+    controller.show_manual()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
---
+++ manual_window.py
@@ -0,0 +1,28 @@
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton
+
+
+class ManualWindow(QWidget):
+
+    switch_window = QtCore.pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.title = "Manual mode"
+        self.left = 100
+        self.top = 100
+        self.width = 600
+        self.height = 380
+        self.init_ui()
+
+    def init_ui(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.switch_btn = QPushButton('&+', self)
+        self.switch_btn.setToolTip('Dodawanie')
+        self.switch_btn.clicked.connect(self.switch)
+
+    def switch(self):
+        self.switch_window.emit()
+        self.close()

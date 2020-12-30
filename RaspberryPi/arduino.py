6e8cc337ffe0931186afc46a09603f5818d7b218
isus-ipanienko <57775222+isus-ipanienko@users.noreply.github.com>
2020-12-30 13:38:27 +0100

    finished text recognition

--- RaspberryPi/arduino.py
+++ RaspberryPi/arduino.py
@@ -1,6 +1,29 @@
 import serial
 import pytesseract
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QLabel, QPushButton, QDialog, QVBoxLayout
 
 global arduino
-arduino = serial.Serial("COM12", 9600)
+arduino = serial.Serial("COM6", 9600)
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Users\\pawel\\Desktop\\tess\\tesseract.exe'
+
+
+class InformationDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(InformationDialog, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle("Information window")
+        # here goes text to information window
+        self.text = QLabel("TEST", self)
+
+        self.button = QPushButton('OK', self)
+        self.button.clicked.connect(self.close)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+    @pyqtSlot()
+    def close(self):
+        self.accept()
